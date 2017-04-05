d552b407417a2a6e3c04200a288ee3435d74c5d2
Craig Minihan <craig@ripcordsoftware.com>
2017-04-05 04:49:28 -0700

    Added the HelloTasks example project

---
+++ examples/HelloTasks/HelloTasks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+using RipcordSoftware.ThreadPool;
+
+namespace HelloTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var pool = new ApplicationThreadPool("test", 16, 1024, true)) 
+            {
+                var tasks = new ApplicationThreadPool.TaskState[pool.MaxThreads];
+                for (var i = 0; i < pool.MaxThreads; ++i) 
+                {
+                    tasks[i] = pool.QueueUserTask(o => Console.WriteLine("Hello from thread {0}", Thread.CurrentThread.ManagedThreadId));
+                }
+
+                ApplicationThreadPool.TaskState.WaitAll(tasks);
+
+                Console.WriteLine("Finished");
+                Console.ReadLine();
+            }
+        }
+    }
+}
